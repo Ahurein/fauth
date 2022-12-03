06e881b8c7f565917e8deef4dc97442ef59d1e0a
Ahurein Ebenezer <ahureinebenezer@gmail.com>
2022-12-03 23:29:45 +0000

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <a href="https://www.facebook.com/v4.0/dialog/oauth?client_id=560985389186186&redirect_uri=https%3A%2F%2F274c-41-66-209-15.eu.ngrok.io%2Fauth%2Fredirect%2Ffacebook&scope=email%2Cuser_friends&response_type=code&auth_type=rerequest&display=popup">
+      <a href="https://www.facebook.com/v4.0/dialog/oauth?client_id=560985389186186&redirect_uri=https%3A%2F%2Ffauth.onrender.com%2Fauth%2Fredirect%2Ffacebook&scope=email%2Cuser_friends&response_type=code&auth_type=rerequest&display=popup">
         Login with facebook
       </a>
     </div>
